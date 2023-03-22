68de6847df52941593648072737c43aca4970118
Anthony Elison <tonyelison37@gmail.com>
2023-03-22 13:50:35 -0600

    Add simple password confirm validation

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
 const submitBtn = document.querySelector(".cta-container button");
 submitBtn.addEventListener('click', () => {
-  const invalidControls = document.querySelectorAll("form input:invalid");
-  invalidControls.forEach((control) => control.classList.add('error'));
+  validatePassword();
+  addErrorClass(document.querySelectorAll("form input:invalid"));
 });
 
 const formControls = document.querySelectorAll("form input");
 formControls.forEach((control) => control.addEventListener('change', () => {
   if (control.checkValidity()) {
     control.classList.remove('error');
+    removeErrorClass([control]);
   }
 }));
+
+function addErrorClass([...nodes]) {
+  nodes.forEach((control) => control.classList.add('error'));
+}
+
+function removeErrorClass([...nodes]) {
+  nodes.forEach((control) => control.classList.remove('error'));
+}
+
+function validatePassword() {
+  const password = document.querySelector("#password");
+  const passConfirm = document.querySelector("#pass-confirm");
+  if (password.value !== passConfirm.value) {
+    addErrorClass([password, passConfirm]);
+  } else {
+    removeErrorClass([password, passConfirm]);
+  }
+}
