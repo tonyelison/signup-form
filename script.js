b4aecbc94352bd4d19f58438a5fd53a55ab0274e
Anthony Elison <tonyelison37@gmail.com>
2023-03-23 16:21:16 -0600

    Simplify js, remove unnecessary fn declarations

--- script.js
+++ script.js
@@ -1,20 +1,12 @@
 const submitBtn = document.querySelector(".cta-container button");
 submitBtn.addEventListener('click', () => {
-  addErrorClass(document.querySelectorAll("form input:invalid"));
+  const invalidFields = document.querySelectorAll("form input:invalid")
+  invalidFields.forEach((control) => control.classList.add('error'));
 });
 
 const formControls = document.querySelectorAll("form input");
 formControls.forEach((control) => control.addEventListener('change', () => {
   if (control.checkValidity()) {
     control.classList.remove('error');
-    removeErrorClass([control]);
   }
 }));
-
-function addErrorClass([...nodes]) {
-  nodes.forEach((control) => control.classList.add('error'));
-}
-
-function removeErrorClass([...nodes]) {
-  nodes.forEach((control) => control.classList.remove('error'));
-}
